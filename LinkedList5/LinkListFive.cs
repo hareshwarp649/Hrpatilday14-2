41a68936483998b70058d777197a046bd7d100a2
Harry <hareshwarp649>
2022-03-06 12:21:39 +0530

    Delete the First element in the linked list

---
+++ LinkedList5/LinkListFive.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrucLinkFiv
+{
+     class LinkListFive
+    {
+
+
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = (Node)temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+
+        public void AddFirst(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = (Node)temp.next;
+                }
+                node.next = temp;
+                head = temp;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+
+        public void AddLast(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = (Node)temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+
+        public int Length()
+        {
+            Node temp;
+            int Count = 0;
+            temp = this.head;
+            while (temp != null)
+            {
+                Count++;
+                temp = (Node)temp.next;
+            }
+            return Count;
+        }
+
+        public void AddAfter(int Atom, int data)
+        {
+            Node temp;
+            Node p;
+            int bom = Atom, i = 1;
+            int leng = this.Length();
+            if (bom > leng)
+            {
+                Console.WriteLine("Invalid Location");
+                Console.WriteLine("Current list having ", leng);
+            }
+            else
+            {
+                p = this.head;
+                while (i < bom)
+                {
+                    p = (Node)p.next;
+                    i++;
+                }
+                Node node = new Node(data);
+                temp = node;
+                temp.next = p.next;
+                Console.WriteLine("{0} inserted into after {1} linked list", node.data, p.data);
+                p.next = temp;
+            }
+        }
+        public void Pop()
+        {
+            Node temp;
+            if (this.head == null)
+            {
+                Console.WriteLine("No elements");
+            }
+            else
+            {
+                temp = this.head;
+               
+                Console.WriteLine(temp.data);
+                head = (Node)head.next;
+                temp.next = null;
+            }
+        }
+    }
+}
---
+++ LinkedList5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrucLinkFiv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkListFive val = new LinkListFive();
+            val.AddLast(56);
+            val.AddLast(30);
+            val.AddLast(70);
+            val.AddAfter(2, 40);
+            int a = val.Length();
+            Console.WriteLine(a);
+            val.Pop();
+        }
+    }
+}
