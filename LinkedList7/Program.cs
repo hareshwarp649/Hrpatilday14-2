bbcc8a88e07f807408230e9f5a83a9e5f8b90079
Harry <hareshwarp649>
2022-03-06 12:43:54 +0530

    Search element in the linked list

---
+++ LinkedList7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrucLinkSeven
+{
+    internal class Program
+    {
+        static void Main(string[] args)          
+        {
+
+            LinkSeven val = new LinkSeven();
+            val.AddLast(56);
+            val.AddLast(30);
+            val.AddLast(70);
+            val.AddAfter(2, 40);
+            val.Find(30);
+
+        }
+    }
+}
