c6cddb142f6d37d10a7fd7ccb792e2618b55b718
Harry <hareshwarp649>
2022-03-06 12:58:09 +0530

    Delete the element Sequence in the linked list

---
+++ LinkedList9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructLinkNine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinklistNine val = new LinklistNine();
+            val.AddLast(56);
+            val.AddLast(30);
+            val.AddLast(70);
+            val.InsertAfter(30, 40);
+            val.remove(70);
+        }
+    }
+}
