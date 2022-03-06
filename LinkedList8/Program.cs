6185dd794e43cd0ac98007547485426c999cc512
Harry <hareshwarp649>
2022-03-06 12:51:44 +0530

    Insert the value Sequence in the linked list

---
+++ LinkedList8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructLinkEight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkListEight value = new LinkListEight();
+            value.AddLast(56);
+            value.AddLast(30);
+            value.AddLast(70);
+            value.InsertAfter(56, 40);
+
+        }
+    }
+}
